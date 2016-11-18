28a9e037422836b9fc1d7d299129a26dc92521ea
Nazmul Hussain <shadhinrcrg@gmail.com>
2016-11-19 00:05:49 +0600

    initialize commit

---
+++ src/main/java/com/nazmulpro/schoolNoticeBoard/AllSchoolNoticeBoardApplication.java
@@ -0,0 +1,12 @@
+package com.nazmulpro.schoolNoticeBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSchoolNoticeBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllSchoolNoticeBoardApplication.class, args);
+	}
+}
